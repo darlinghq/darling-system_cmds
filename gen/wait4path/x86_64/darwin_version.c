6526aaace3c1e66131526e888d673dc5b1edeaf4
Thomas A <curioustommy@protonmail.com>
2023-02-23 21:50:52 -0800

    Generate Sources for system_cmds-880.120.1
    I had to generate the wait4path on a real Mac since Darling's reimplementation of PlistBuddy is incomplete.

---
+++ gen/wait4path/x86_64/darwin_version.c
@@ -0,0 +1,11 @@
+__attribute__((__used__)) const unsigned long __darwin_builder_version = 1320;
+__attribute__((__used__)) const unsigned long __darwin_builder_build = 0x1A1;
+__attribute__((__used__)) const char *__darwin_build_inc_version = "880.120.1";
+__attribute__((__used__)) const char *__darwin_version_string = "Darwin Filesystem Path Waiter Version 1.0.0: Thu Feb 23 21:46:24 PST 2023; thomasa:";
+__attribute__((__used__)) const char *__darwin_variant = "RELEASE";
+__attribute__((__used__)) const char *__darwin_version_string_heywhat = "@(#)VERSION:Darwin Filesystem Path Waiter Version 1.0.0: Thu Feb 23 21:46:24 PST 2023; thomasa:";
+__attribute__((__used__)) const char *__darwin_debug_binary = "";
+__attribute__((__used__))
+__attribute__((__section__("__TEXT,__info_plist")))
+static const char __darwin_info_plist[] = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"><plist version=\"1.0\"><dict>	<key>CFBundleDisplayName</key>	<string>Darwin Filesystem Path Waiter</string>	<key>CFBundleExecutable</key>	<string>wait4path</string>	<key>CFBundleIdentifier</key>	<string>com.apple.system_cmds.wait4path</string>	<key>CFBundleInfoDictionaryVersion</key>	<string>6.0</string>	<key>CFBundleName</key>	<string>wait4path</string>	<key>CFBundleShortVersionString</key>	<string>1.0.0</string>	<key>CFBundleVersion</key>	<string>880.120.1</string>	<key>DTCompiler</key>	<string></string>	<key>DTPlatformName</key>	<string></string>	<key>DTPlatformVersion</key>	<string></string>	<key>DTSDKBuild</key>	<string></string>	<key>DTSDKName</key>	<string></string>	<key>DTXcode</key>	<string></string>	<key>DTXcodeBuild</key>	<string></string>	<key>DarwinBuilderBuild</key>	<string>1A1</string>	<key>DarwinBuilderVersion</key>	<string>13.2.1</string>	<key>DarwinVariant</key>	<string>RELEASE</string>	<key>NSHumanReadableCopyright</key>	<string>Copyright 2015 Apple Inc. All rights reserved.</string></dict></plist>";
+
